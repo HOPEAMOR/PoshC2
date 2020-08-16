16cae219e35b343efa1bab1d67e92606d47207f8
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-08-16 14:26:29 +0100

    Fix FPC

--- resources/scripts/fpc.py
+++ resources/scripts/fpc.py
@@ -13,13 +13,13 @@ class Colours:
 
 def create_arg_parser():
     parser = argparse.ArgumentParser(description='Find Posh Command - Search for a PoshC2 Command Output')
-    parser.add_argument("-p", "--project", help='The PoshC2 project dir', default = '/opt/PoshC2_Project')
-    parser.add_argument("-d", "--database_type", help="The database type (SQLite/Postgres)", default = 'SQLite')
-    parser.add_argument("-pg", "--postgres_string", help="The postgres connection string (if using postgres)", default = '')
-    parser.add_argument("-c", "--command", help='The command to search for', default = '%')
-    parser.add_argument("-u", "--user", help='The user to filter on', default = '%')
-    parser.add_argument("-o", "--output", help='The output to search for', default = '%')
-    parser.add_argument("-t", "--taskid", help='The taskid to search for', default = '%')
+    parser.add_argument("-p", "--project", help='The PoshC2 project dir', default='/opt/PoshC2_Project')
+    parser.add_argument("-d", "--database_type", help="The database type (SQLite/Postgres)", default='SQLite')
+    parser.add_argument("-pg", "--postgres_string", help="The postgres connection string (if using postgres)", default='')
+    parser.add_argument("-c", "--command", help='The command to search for', default='%')
+    parser.add_argument("-u", "--user", help='The user to filter on', default='%')
+    parser.add_argument("-o", "--output", help='The output to search for', default='%')
+    parser.add_argument("-t", "--taskid", help='The taskid to search for', default='%')
     return parser
 
 
